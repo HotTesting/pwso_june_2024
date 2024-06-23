eb46af2c4153d0a191297d2328b3d2d20fc6adf3
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2024-06-23 18:52:45 +0300

    Update admin.test.ts

--- tests/admin.test.ts
+++ tests/admin.test.ts
@@ -1,13 +1,17 @@
 import { shopTest } from "../fixture";
 
-shopTest('Admin should see additional items in dashboard', {
-    annotation: [{type: 'BUG', description: 'http://jira.com/CS-123'}]
-},async ({ app, newAdminUser }) => {
+shopTest(
+  "Admin should see additional items in dashboard",
+  {
+    annotation: [{ type: "BUG", description: "http://jira.com/CS-123" }],
+  },
+  async ({ app, newAdminUser }) => {
     await app.accountDetails.open();
-    await app.accountDetails.expectMenuItemVisible('Merchant')
-    await app.accountDetails.expectMenuItemVisible('Users')
-    await app.accountDetails.expectMenuItemVisible('Orders')
-    await app.accountDetails.expectMenuItemVisible('Reviews')
-    await app.accountDetails.expectMenuItemVisible('WishList')
-    await app.accountDetails.expectMenuItemVisible('Support')
-});
+    await app.accountDetails.expectMenuItemVisible("Merchant");
+    await app.accountDetails.expectMenuItemVisible("Users");
+    await app.accountDetails.expectMenuItemVisible("Orders");
+    await app.accountDetails.expectMenuItemVisible("Reviews");
+    await app.accountDetails.expectMenuItemVisible("WishList");
+    await app.accountDetails.expectMenuItemVisible("Support");
+  }
+);
